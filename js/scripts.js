ea9765ad04c55a3805727b349f9441d9d5a21f74
Joel Delight and Patrick Lipscomb <patrick@>
2016-06-02 15:20:56 -0700

    create triangle indicating website using global functions and a form, more to come.

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+var equilateral = function(side1, side2, side3) {
+  if (side1 === side2 && side1 === side3) {
+  return true;
+} else {
+  return false;
+}
+}
+
+var isosceles = function(side1, side2, side3) {
+  if (side1 === side2 || side1 === side3 || side2 === side3) {
+  return true;
+} else {
+  return false;
+}
+}
+
+var scalene = function(side1, side2, side3) {
+  if (side1 != side2 && side1 != side3 && side2 != side3) {
+  return true;
+} else {
+  return false;
+}
+}
+
+var triangleCheck = function(side1, side2, side3) {
+  if ( side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1 ) {
+  return false;
+} else {
+  return true;
+}
+}
+
+$(function() {
+  $('form#triangle').submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    if ( !triangleCheck(side1, side2, side3) ) {
+      alert("this is not a triangle");
+    } else {
+      if ( scalene(side1, side2, side3) ) {
+        alert("this is a scalene");
+      } else if ( equilateral(side1, side2, side3) ) {
+          alert("this is a equilateral");
+        } else if ( isosceles(side1, side2, side3) ) {
+          alert("This is a isosceles.");
+        }
+    }
+    event.preventDefault();
+  });
+});
