042eefabf6d224a0f1b254a950aabcd653297c10
Joel Delight and Patrick Lipscomb <joel@>
2016-06-02 16:01:04 -0700

    fix some indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -35,23 +35,18 @@ $(function() {
     var side1 = parseInt($("input#side1").val());
     var side2 = parseInt($("input#side2").val());
     var side3 = parseInt($("input#side3").val());
-
     $(".scalene").hide();
     $(".equilateral").hide();
-    $(".isosceles").hide();
-
+    $(".isosceles").hide()
     if ( !triangleCheck(side1, side2, side3) ) {
       alert("this is not a triangle");
     } else {
       if ( scalene(side1, side2, side3) ) {
         $(".scalene").show();
-
       } else if ( equilateral(side1, side2, side3) ) {
         $(".equilateral").show();
-
         } else if ( isosceles(side1, side2, side3) ) {
           $(".isosceles").show();
-
         }
     }
     event.preventDefault();
